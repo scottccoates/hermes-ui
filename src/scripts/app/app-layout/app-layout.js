d2b7e1ca316ca1238788854d6ff54d457abecaf1
scott coates <scoarescoare@gmail.com>
2015-03-18 17:05:38 -0500

    running in sequence

--- src/scripts/app/app-layout/app-layout.js
+++ src/scripts/app/app-layout/app-layout.js
@@ -1,24 +1,32 @@
 'use strict';
 
 import React from 'react';
+import ReactAddons from 'react/addons';
+import Router from 'react-router';
+
 import Sidebar from './sidebar/sidebar';
 import Header from './header/header';
 import Footer from './footer/footer';
 
-import Router from 'react-router';
-
 const {RouteHandler} = Router;
+const ReactCSSTransitionGroup = ReactAddons.addons.CSSTransitionGroup;
 
 const AppLayout = React.createClass({
+  mixins: [Router.State],
 
-  render: function () {
+  render() {
+    //https://github.com/rackt/react-router/blob/e0a15ebc81d76119935fef27e0ab7d7b024b98fd/examples/animations/app.js
+    var name = this.getRoutes().reverse()[0].name;
+
     return (
         <div id="page-wrapper">
           <Sidebar />
           <div id="main-wrapper">
             <Header />
             <div id="content-wrapper">
-              <RouteHandler/>
+              <ReactCSSTransitionGroup transitionName="example">
+                <RouteHandler key={name}/>
+              </ReactCSSTransitionGroup>
             </div>
             <Footer />
           </div>
