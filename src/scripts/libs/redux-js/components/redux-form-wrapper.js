7e315fdbae15b2259509f2c879aca31a357dabfc
scott coates <scoarescoare@gmail.com>
2016-03-29 16:31:07 -0500

    fix issue with form not show newly created agreement types.

--- src/scripts/libs/redux-js/components/redux-form-wrapper.js
+++ src/scripts/libs/redux-js/components/redux-form-wrapper.js
@@ -10,24 +10,34 @@ export default function formWrapper(wrapped, defaults) {
   const component = React.createClass({
     displayName: "FormWrapperComponent",
 
-    shouldComponentUpdate: function (nextProps, nextState) {
-      let retVal = true;
-      // https://facebook.github.io/react/docs/component-specs.html#updating-shouldcomponentupdate
-      // this prevents the form from being "reset" if someone
-      // is editing it and that agreement data is concurrently changed in firebase. it'd wipe out their work.
-      // we probably don't need realtime sync on an edit form.
-      if (nextProps.initialValues && nextProps.initialValues.id) {
-        retVal = nextProps.initialValues.id !== this.props.initialValues.id;
+    getInitialState() {
+
+      // assume that if ID is provided then all values have been provided.
+      const retVal = {};
+
+      if (this.props.initialValues && this.props.initialValues.id) {
+        retVal.initialValues = this.props.initialValues;
       }
 
       return retVal;
     },
 
-    render() {
-      const initialValuesWithDefaults = Object.assign({}, defaults, this.props.initialValues);
+    componentWillReceiveProps: function (nextProps) {
+      // https://facebook.github.io/react/docs/component-specs.html#updating-shouldcomponentupdate
+      // this prevents the form from being "reset" if someone
+      // is editing it and that agreement data is concurrently changed in firebase. it'd wipe out their work.
+      // we probably don't need realtime sync on an edit form.
+      if (!this.state.initialValues && nextProps.initialValues && nextProps.initialValues.id) {
+        this.setState({initialValues: nextProps.initialValues});
+      }
+    },
 
-      const newProps = Object.assign({}, this.props, {initialValues: initialValuesWithDefaults});
-      return <WrappedElement {...newProps}/>;
+    render() {
+      const initialValuesWithDefaults = Object.assign({}, defaults, this.state.initialValues);
+      const newProps                  = Object.assign({}, this.props, {initialValues: initialValuesWithDefaults});
+      const retVal                    = <WrappedElement {...newProps}/>;
+
+      return retVal;
     }
   });
 
